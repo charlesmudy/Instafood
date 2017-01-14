37993e9896fceb68d3e303dae665fba6db49105b
costin <costinaldea@yahoo.co.uk>
2017-01-14 13:09:59 +0200

    /food-mixer /food-mixer-enhanced

--- js/controllers.js
+++ js/controllers.js
@@ -37,7 +37,7 @@ function translateCtrl($translate, $scope) {
 
 function UserService($log, sessionFactory) {
     var userService = this;
-    var user = { name: ''};
+    var user = {name: ''};
     this.user = {name: ''}
 }
 
@@ -47,7 +47,7 @@ var mainController = function AssetController($scope, userService) {
     $scope.password = "password";
 
     $scope.loginUser = function () {
-        var user =  sessionFactory.retrieveUserAssets($scope.username, $scope.password);
+        var user = sessionFactory.retrieveUserAssets($scope.username, $scope.password);
         $scope.user = user;
         $scope.userService.user = user;
         console.log($scope.userService.user)
@@ -66,21 +66,21 @@ angular
 
 
         $scope.loginUser = function () {
-            var user =  sessionFactory.retrieveUserAssets($scope.username, $scope.password);
+            var user = sessionFactory.retrieveUserAssets($scope.username, $scope.password);
             $scope.user = user;
             $scope.userService.user = user;
             console.log($scope.userService.user)
         }
 
-        $scope.assetData = function() {
-             sessionFactory.retrieveUserAssets($scope.username, $scope.password).success(function (data) {
+        $scope.assetData = function () {
+            sessionFactory.retrieveUserAssets($scope.username, $scope.password).success(function (data) {
                 $scope.assetdata = data;
-                 var total = 0;
-                 for (var i = 0; i < $scope.assetdata.assets.length; i++) {
-                     total = total + $scope.assetdata.assets[i].amount;
-                 }
+                var total = 0;
+                for (var i = 0; i < $scope.assetdata.assets.length; i++) {
+                    total = total + $scope.assetdata.assets[i].amount;
+                }
 
-                 $scope.assetdata.total = total;
+                $scope.assetdata.total = total;
             })
         }
         $scope.assetData()
@@ -93,7 +93,7 @@ angular
 
         $scope.marketdata = []
 
-        $scope.marketData = function() {
+        $scope.marketData = function () {
             $scope.marketdata = sessionFactory.getMarketData().success(function (data) {
                 $scope.marketdata = data
             })
@@ -101,110 +101,24 @@ angular
 
         $scope.marketData()
 
-     })
-     .controller('ClaimController', function ClaimController($scope, $state, $http) {
-    $scope.providers = [];
-
-    this.init = function() {
-        console.log('ClaimController.init()');
-        $http({
-            method: 'GET',
-            url: 'http://travelcoin-api.herokuapp.com/providers'
-        }).then(function(response) {
-            $scope.providers = response.data;
-        });
-    }
-
-    this.init();
     })
-     .service('userService', UserService)
+    .controller('ClaimController', function ClaimController($scope, $state, $http) {
+        $scope.providers = [];
 
-    .factory('Base64', function () {
-        /* jshint ignore:start */
+        this.init = function () {
+            console.log('ClaimController.init()');
+            $http({
+                method: 'GET',
+                url: 'data/providers.js'
+            }).then(function (response) {
+                $scope.providers = response.data;
+            });
+        }
 
-        var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-
-        return {
-            encode: function (input) {
-                var output = "";
-                var chr1, chr2, chr3 = "";
-                var enc1, enc2, enc3, enc4 = "";
-                var i = 0;
-
-                do {
-                    chr1 = input.charCodeAt(i++);
-                    chr2 = input.charCodeAt(i++);
-                    chr3 = input.charCodeAt(i++);
-
-                    enc1 = chr1 >> 2;
-                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-                    enc4 = chr3 & 63;
-
-                    if (isNaN(chr2)) {
-                        enc3 = enc4 = 64;
-                    } else if (isNaN(chr3)) {
-                        enc4 = 64;
-                    }
-
-                    output = output +
-                        keyStr.charAt(enc1) +
-                        keyStr.charAt(enc2) +
-                        keyStr.charAt(enc3) +
-                        keyStr.charAt(enc4);
-                    chr1 = chr2 = chr3 = "";
-                    enc1 = enc2 = enc3 = enc4 = "";
-                } while (i < input.length);
-
-                return output;
-            },
-
-            decode: function (input) {
-                var output = "";
-                var chr1, chr2, chr3 = "";
-                var enc1, enc2, enc3, enc4 = "";
-                var i = 0;
-
-                // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-                var base64test = /[^A-Za-z0-9\+\/\=]/g;
-                if (base64test.exec(input)) {
-                    window.alert("There were invalid base64 characters in the input text.\n" +
-                        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-                        "Expect errors in decoding.");
-                }
-                input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-                do {
-                    enc1 = keyStr.indexOf(input.charAt(i++));
-                    enc2 = keyStr.indexOf(input.charAt(i++));
-                    enc3 = keyStr.indexOf(input.charAt(i++));
-                    enc4 = keyStr.indexOf(input.charAt(i++));
-
-                    chr1 = (enc1 << 2) | (enc2 >> 4);
-                    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                    chr3 = ((enc3 & 3) << 6) | enc4;
-
-                    output = output + String.fromCharCode(chr1);
-
-                    if (enc3 != 64) {
-                        output = output + String.fromCharCode(chr2);
-                    }
-                    if (enc4 != 64) {
-                        output = output + String.fromCharCode(chr3);
-                    }
-
-                    chr1 = chr2 = chr3 = "";
-                    enc1 = enc2 = enc3 = enc4 = "";
-
-                } while (i < input.length);
-
-                return output;
-            }
-        };
-
-        /* jshint ignore:end */
+        this.init();
     })
-    .factory('sessionFactory', function ($http, Base64) {
+    .service('userService', UserService)
+    .factory('sessionFactory', function ($http) {
 
         var factory = {};
         $http.defaults.useXDomain = true;
@@ -216,19 +130,21 @@ angular
 
         factory.retrieveUserAssets = function (username, password) {
             return $http({
-                method: 'POST',
+                method: 'GET',
                 url: 'http://travelcoin-api.herokuapp.com/authenticate',
+                url: 'data/assets.json',
                 //url: 'http://localhost:8080/authenticate',
-                data: $.param({email:username}),
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                //data: $.param({email: username}),
+                //headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             });
+
         };
 
 
-        factory.getMarketData = function() {
+        factory.getMarketData = function () {
             return $http({
                 method: 'GET',
-                url: 'http://travelcoin-api.herokuapp.com/products'
+                url: 'data/products.json'
                 //url: 'http://localhost:8080/products'
             });
         };
